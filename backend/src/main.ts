8ad0d0e82081ca0c1eddd490116eaf366afb300e
Raphael Brasil <raphaelbrasil7@gmail.com>
2024-10-18 14:44:49 -0300

    feat: :art: Ajusta os arquivos docker

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,14 +7,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Client API')
+    .setTitle('Teddy Client API')
     .setDescription('API for managing clients')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(4000);
 }
 
 bootstrap();
