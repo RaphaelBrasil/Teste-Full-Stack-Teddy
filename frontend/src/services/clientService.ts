ab8c0d0fb7476a2fcd452ed1bf23abf5fead7fab
RaphaelBrasil <raphaelbrasil7@gmail.com>
2024-10-20 19:04:44 -0300

    feat: :rocket: Finaliza o desenvolvimento das partes de integração com docker

--- frontend/src/services/clientService.ts
+++ frontend/src/services/clientService.ts
@@ -5,23 +5,23 @@ import { Client } from "../types/clientTypes";
 const API_URL = import.meta.env.VITE_API_URL || "http://localhost:4000/clients";
 
 export const getClients = async (): Promise<Client[]> => {
-  const response = await axios.get(API_URL);
-  return response.data;
+	const response = await axios.get(API_URL);
+	return response.data;
 };
 
 export const createClient = async (client: Client): Promise<Client> => {
-  const response = await axios.post(API_URL, client);
-  return response.data;
+	const response = await axios.post(API_URL, client);
+	return response.data;
 };
 
 export const deleteClient = async (id: string): Promise<void> => {
-  await axios.delete(`${API_URL}/${id}`);
+	await axios.delete(`${API_URL}/${id}`);
 };
 
 export const updateClient = async (
-  id: string,
-  client: Partial<Client>
+	id: string,
+	client: Partial<Client>
 ): Promise<Client> => {
-  const response = await axios.put(`${API_URL}/${id}`, client);
-  return response.data;
+	const response = await axios.put(`${API_URL}/${id}`, client);
+	return response.data;
 };
